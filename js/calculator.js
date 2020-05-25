f90f1422b3fe929b02581f3c830f2ade38b2acae
adlamas <alan12394@hotmail.com>
2020-05-25 16:26:35 -0300

    added html and calculator.js

--- js/calculator.js
+++ js/calculator.js
@@ -1,25 +1,41 @@
 
-/*
-  $(".magnitud").click(function(){
-  button_id = this.id;
-  button_text = $(this).text();
-  $.ajax({
-    url: "js/" + button_id + ".js",
-    method: "GET",
-  }).done(function(data) {
-    $('.modal-title').text(JSON.parse(data)['nombre']);
-    console.log(data);
-    $('#body-descripcion').text(JSON.parse(data)['descripcion']);
-    $('#img-magnitud').attr('src', 'img/' + button_id + '.jpg');
-    ecuaciones = JSON.parse(data)['ecuaciones'];
-    $('#body-ecuaciones').text(
-      'Ecuaciones en las que se utiliza: ' + ecuaciones
-    );
+import * as calculosDalton from '/calculos/dalton';
+
+
+$("#calculadoraLeyDeBoyle").click(function(){
+  $("#cuerpo_calculadora").html(""+
+  "<input type='number' placeholder='500' id='BoyleNumero1'>"+
+  "<input type='number' placeholder='300' id='BoyleNumero2'>"+
+  "<button class='btn btn-primary' id='calcularLeyDeBoyle'>Calcular Boyle</button>"+
+  "");
+
+  calculosDalton.alertar();
+});
+
+$("#calculadoraLeyDeCharles").click(function(){
+  $("#cuerpo_calculadora").html(""+
+  "<input type='number' placeholder='500' id='CharlesNumero1'>"+
+  "<input type='number' placeholder='300' id='CharlesNumero2'>"+
+  "<button class='btn btn-primary' id='calcularLeyDeCharles'>Calcular Charles</button>"+
+  "");
+
+  $(document).ready(function(){
+    calcularLeyDeCharles();
   });
 });
-*/
 
-
-$("#boyle_c").click(function(){
-  $("#boyle_text").append('holis');
+$("#calcularLeyDeBoyle").click(function(){
+  var numero1 = parseInt($("#BoyleNumero1").val());
+  var numero2 = parseInt($("#BoyleNumero2").val());
+  var numeroFinal = numero1 + numero2;
+  $("#cuerpo_calculadora").append('<p>La suma de los dos numeros es: ' + numeroFinal + "</p>");
 });
+
+function calcularLeyDeCharles(){
+  $("#calcularLeyDeDalton").click(function(){
+    var numero1 = parseInt($("#DaltonNumero1").val());
+    var numero2 = parseInt($("#DaltonNumero2").val());
+    var numeroFinal = numero1 / numero2;
+    $("#cuerpo_calculadora").append('<p>La suma de los dos numeros para Dalton es: ' + numeroFinal + "</p>");
+  });
+}
---
+++ js/calculos/dalton.js
@@ -0,0 +1,28 @@
+
+$('#presionInicial').click(function(){
+  var presionFinal = parseInt($("presionFinal").val());
+  var volumenInicial = parseInt($("volumenInicial").val());
+  var volumenFinal = parseInt($("volumenFinal").val());
+
+});
+
+$('#presionFinal').click(function(){
+  var presionInicial = parseInt($("presionInicial").val());
+  var volumenInicial = parseInt($("volumenInicial").val());
+  var volumenFinal = parseInt($("volumenFinal").val());
+
+});
+
+$('#volumenInicial').click(function(){
+  var presionFinal = parseInt($("presionFinal").val());
+  var presionInicial = parseInt($("presionInicial").val());
+  var volumenFinal = parseInt($("volumenFinal").val());
+
+});
+
+$('#volumenFinal').click(function(){
+  var presionFinal = parseInt($("presionFinal").val());
+  var volumenInicial = parseInt($("volumenInicial").val());
+  var presionInicial = parseInt($("presionInicial").val());
+
+});
