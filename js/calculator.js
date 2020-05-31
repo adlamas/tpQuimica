a8e7dd2bc8fa67eb48b2c322ce61a9676a250a37
adlamas <alan12394@hotmail.com>
2020-05-30 21:28:30 -0300

    added some styles to html

--- js/calculator.js
+++ js/calculator.js
@@ -6,15 +6,16 @@ $.getScript("/js/calculos/ideales.js", function() {});
 
 $("#calculadoraLeyDeBoyle").click(function(){
   $("#cuerpo_calculadora").html(""+
-  "Presion Inicial(ATM) <input type='number' placeholder='500' id='presionInicialBoyle' step='any'><br>"+
-  "Presion Final(ATM) <input type='number' placeholder='300' id='presionFinalBoyle' step='any'><br>"+
-  "Volumen Inicial(L) <input type='number' placeholder='300' id='volumenInicialBoyle' step='any'><br>"+
-  "Volumen Final(L) <input type='number' placeholder='300' id='volumenFinalBoyle' step='any'><br><br>"+
+  "<h2>Calcular ley de Boyle</h2><br>" +
+  "<div>Presion Inicial(ATM) <input type='number' placeholder='1' id='presionInicialBoyle' step='any'></div><br>"+
+  "<div>Presion Final(ATM) <input type='number' placeholder='2' id='presionFinalBoyle' step='any'></div><br>"+
+  "<div>Volumen Inicial(L) <input type='number' placeholder='2' id='volumenInicialBoyle' step='any'></div><br>"+
+  "<div>Volumen Final(L) <input type='number' placeholder='1,5' id='volumenFinalBoyle' step='any'></div><br><br>"+
 
-  "<button class='btn btn-primary' id='calcularPresionInicialBoyle'>Presion inicial</button><br><br>"+
-  "<button class='btn btn-primary' id='calcularPresionFinalBoyle'>Presion final</button><br><br>"+
-  "<button class='btn btn-primary' id='calcularVolumenInicialBoyle'>Volumen inicial</button><br><br>"+
-  "<button class='btn btn-primary' id='calcularVolumenFinalBoyle'>Volumen final</button><br><br>"+
+  "<button class='btn btn-primary' id='calcularPresionInicialBoyle'>Calcular presion inicial</button><br><br>"+
+  "<button class='btn btn-primary' id='calcularPresionFinalBoyle'>Calcular presion final</button><br><br>"+
+  "<button class='btn btn-primary' id='calcularVolumenInicialBoyle'>Calcular volumen inicial</button><br><br>"+
+  "<button class='btn btn-primary' id='calcularVolumenFinalBoyle'>Calcular volumen final</button><br><br>"+
   "");
 
   $(document).ready(function(){
@@ -26,15 +27,16 @@ $("#calculadoraLeyDeBoyle").click(function(){
 
 $("#calculadoraLeyDeCharles").click(function(){
   $("#cuerpo_calculadora").html(""+
-  "Temperatura Inicial(K) <input type='number' placeholder='500' id='temperaturaInicialCharles' step='any'><br>"+
-  "Temperatura Final(K) <input type='number' placeholder='300' id='temperaturaFinalCharles' step='any'><br>"+
-  "Volumen Inicial(L) <input type='number' placeholder='300' id='volumenInicialCharles' step='any'><br>"+
-  "Volumen Final(L) <input type='number' placeholder='300' id='volumenFinalCharles' step='any'><br><br>"+
+  "<h2>Calcular ley de Charles</h2><br>" +
+  "<div>Temperatura Inicial(K) <input type='number' placeholder='500' id='temperaturaInicialCharles' step='any'></div><br>"+
+  "<div>Temperatura Final(K) <input type='number' placeholder='350' id='temperaturaFinalCharles' step='any'></div><br>"+
+  "<div>Volumen Inicial(L) <input type='number' placeholder='2' id='volumenInicialCharles' step='any'></div><br>"+
+  "<div>Volumen Final(L) <input type='number' placeholder='1,5' id='volumenFinalCharles' step='any'></div><br><br>"+
 
-  "<button class='btn btn-primary' id='calcularTemperaturaInicialCharles'>Temperatura inicial</button><br><br>"+
-  "<button class='btn btn-primary' id='calcularTemperaturaFinalCharles'>Temperatura final</button><br><br>"+
-  "<button class='btn btn-primary' id='calcularVolumenInicialCharles'>Volumen inicial</button><br><br>"+
-  "<button class='btn btn-primary' id='calcularVolumenFinalCharles'>Volumen final</button><br><br>"+
+  "<button class='btn btn-primary' id='calcularTemperaturaInicialCharles'>Calcular temperatura inicial</button><br><br>"+
+  "<button class='btn btn-primary' id='calcularTemperaturaFinalCharles'>Calcular temperatura final</button><br><br>"+
+  "<button class='btn btn-primary' id='calcularVolumenInicialCharles'>Calcular volumen inicial</button><br><br>"+
+  "<button class='btn btn-primary' id='calcularVolumenFinalCharles'>Calcular volumen final</button><br><br>"+
   "");
 
   $(document).ready(function(){
@@ -46,15 +48,16 @@ $("#calculadoraLeyDeCharles").click(function(){
 
 $("#calculadoraLeyDeGasesIdeales").click(function(){
   $("#cuerpo_calculadora").html(""+
-  "Numero de moles <input type='number' placeholder='2300' id='numeroMolesGasesIdeales' step='any'><br>"+
-  "Volumen(L) <input type='number' placeholder='300' id='volumenGasesIdeales' step='any'><br>"+
-  "Presion(ATM) <input type='number' placeholder='300' id='presionGasesIdeales' step='any'><br>"+
-  "Temperatura(K) <input type='number' placeholder='300' id='temperaturaGasesIdeales' step='any'><br>"+
+  "<h2>Calcular ley de Gases ideales</h2><br>" +
+  "<div>Numero de moles <input type='number' placeholder='2' id='numeroMolesGasesIdeales' step='any'></div><br>"+
+  "<div>Volumen(L) <input type='number' placeholder='1,5' id='volumenGasesIdeales' step='any'></div><br>"+
+  "<div>Presion(ATM) <input type='number' placeholder='2' id='presionGasesIdeales' step='any'></div><br>"+
+  "<div>Temperatura(K) <input type='number' placeholder='450' id='temperaturaGasesIdeales' step='any'></div><br>"+
 
-  "<button class='btn btn-primary' id='calcularVolumenGasesIdeales'>Volumen </button><br><br>"+
-  "<button class='btn btn-primary' id='calcularNumeroMolesGasesIdeales'>Numero moles</button><br><br>"+
-  "<button class='btn btn-primary' id='calcularPresionGasesIdeales'>Volumen </button><br><br>"+
-  "<button class='btn btn-primary' id='calcularTemperaturaGasesIdeales'>Numero moles</button><br><br>"+
+  "<button class='btn btn-primary' id='calcularNumeroMolesGasesIdeales'>Calcular numero moles</button><br><br>"+
+  "<button class='btn btn-primary' id='calcularTemperaturaGasesIdeales'>Calcular temperatura</button><br><br>"+
+  "<button class='btn btn-primary' id='calcularPresionGasesIdeales'>Calcular presion </button><br><br>"+
+  "<button class='btn btn-primary' id='calcularVolumenGasesIdeales'>Calcular volumen </button><br><br>"+
   "");
 
   $(document).ready(function(){
