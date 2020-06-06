eff98674001e09a6887701ed39cf3916c51adc4b
adlamas <alan12394@hotmail.com>
2020-06-06 19:01:39 -0300

    updated boyle.js file, refactored

--- js/calculos/boyle.js
+++ js/calculos/boyle.js
@@ -1,38 +1,80 @@
 $.getScript("/js/calculos/chequearValores.js", function() {});
 
-/*function obtenerValoresBoyle(){
+function obtenerValoresBoyle(){
   var presionFinal = parseFloat($("#presionFinalBoyle").val());
+  var presionInicial = parseFloat($("#presionInicialBoyle").val());
   var volumenInicial = parseFloat($("#volumenInicialBoyle").val());
   var volumenFinal = parseFloat($("#volumenFinalBoyle").val());
 
-  return [presionFinal, volumenInicial, volumenFinal];
+  return { pf: presionFinal, pi: presionInicial, vi: volumenInicial, vf: volumenFinal };
 };
 
-function calcularPresionInicialBoyle(magnitudes){
-  if(chequearValores([presionFinal, volumenInicial, volumenFinal]) != true){
+function calcularMagnitudesBoyle(magnitudes){
+  if(chequearValores(magnitudes) != true){
     alert("Un valor no se ha cargado correctamente, por favor intentar de nuevo");
-    throw new Error("ghola");
+    throw new Error("Un valor no se ha cargado correctamente, por favor intentar de nuevo");
   }
   else{
-    var presionInicial = (presionFinal * volumenFinal) / volumenInicial;
-    return presionInicial;
+    var magnitudCalculada = (magnitudes[0] * magnitudes[1]) / magnitudes[2];
+    return magnitudCalculada;
   }
 }
 
-function last(){
+function presionInicialBoyle(){
   var valores = obtenerValoresBoyle();
-  var resultado = calcularPresionInicialBoyle(valores);
+  var resultado = calcularMagnitudesBoyle(
+    [valores["pf"], valores["vf"], valores["vi"]]
+  );
   alert("La presion inicial es: " + resultado + "ATM");
 }
 
-$('#calcularPresionInicialBoyle').click(function(){
-  last();
-});*/
+function presionFinalBoyle(){
+  var valores = obtenerValoresBoyle();
+  var resultado = calcularMagnitudesBoyle(
+    [valores["pi"], valores["vi"], valores["vf"]]
+  );
+  alert("La presion final es: " + resultado + "ATM");
+}
+
+function volumenInicialBoyle(){
+  var valores = obtenerValoresBoyle();
+  var resultado = calcularMagnitudesBoyle(
+    [valores["pf"], valores["vf"], valores["pi"]]
+  );
+  alert("El volumen inicial es: " + resultado + "L");
+}
+
+function volumenFinalBoyle(){
+  var valores = obtenerValoresBoyle();
+  var resultado = calcularMagnitudesBoyle(
+    [valores["pi"], valores["vi"], valores["pf"]]
+  );
+  alert("El volumen final es: " + resultado + "L");
+}
+
+function calcularBoyle(){
+  $('#calcularPresionInicialBoyle').click(function(){
+    presionInicialBoyle();
+  });
+
+  $('#calcularVolumenInicialBoyle').click(function(){
+    volumenInicialBoyle();
+  });
+
+  $('#calcularPresionFinalBoyle').click(function(){
+    presionFinalBoyle();
+  });
+
+  $('#calcularVolumenFinalBoyle').click(function(){
+    volumenFinalBoyle();
+  });
+}
+
 
 
 // anterior
 
-function calcularBoyle(){
+/*function calcularBoyle(){
   $('#calcularPresionInicialBoyle').click(function(){
     var presionFinal = parseFloat($("#presionFinalBoyle").val());
     var volumenInicial = parseFloat($("#volumenInicialBoyle").val());
@@ -89,4 +131,4 @@ function calcularBoyle(){
       alert("El volumen final es: " + volumenFinal + "L");
     }
   });
-}
+}*/
