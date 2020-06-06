c2b279109593a1b4f3d2412f1fba48be226cb5e8
adlamas <alan12394@hotmail.com>
2020-06-06 20:48:16 -0300

    refactored ideales.js

--- js/calculos/ideales.js
+++ js/calculos/ideales.js
@@ -1,6 +1,82 @@
 $.getScript("/js/calculos/chequearValores.js", function() {});
 
+const R = 0.08205;
+
+function obtenerValoresIdeales(){
+  var volumen = parseFloat($("#volumenGasesIdeales").val());
+  var temperatura = parseFloat($("#temperaturaGasesIdeales").val());
+  var numeroMoles = parseFloat($("#numeroMolesGasesIdeales").val());
+  var presion = parseFloat($("#presionGasesIdeales").val());
+
+  return { vol: volumen, temp: temperatura, nm: numeroMoles, pr: presion };
+};
+
+function calcularMagnitudesIdeales(magnitudes){
+  if(chequearValores(magnitudes) != true){
+    alert("Un valor no se ha cargado correctamente, por favor intentar de nuevo");
+    throw new Error("Un valor no se ha cargado correctamente, por favor intentar de nuevo");
+  }
+  else{
+    var magnitudCalculada = (magnitudes[0] * magnitudes[1] * magnitudes[2] * magnitudes[3])
+    return magnitudCalculada;
+  }
+}
+
+function volumenGasesIdeales(){
+  var valores = obtenerValoresIdeales();
+  var resultado = calcularMagnitudesIdeales([
+    (1/valores["pr"]), valores["temp"], valores["nm"], R
+  ]
+  );
+  alert("El volumen es: " + resultado + "L");
+}
+
+function presionGasesIdeales(){
+  var valores = obtenerValoresIdeales();
+  var resultado = calcularMagnitudesIdeales([
+    (1/valores["vol"]), valores["temp"], valores["nm"], R
+  ]
+  );
+  alert("La presion es: " + resultado + "ATM");
+}
+
+function temperaturaGasesIdeales(){
+  var valores = obtenerValoresIdeales();
+  var resultado = calcularMagnitudesIdeales([
+    valores["vol"], valores["pr"], (1/valores["nm"]), (1/R)
+  ]
+  );
+  alert("La temperatura es: " + resultado + "K");
+}
+
+function numeroMolesGasesIdeales(){
+  var valores = obtenerValoresIdeales();
+  var resultado = calcularMagnitudesIdeales([
+    valores["vol"], valores["pr"], (1/valores["temp"]), (1/R)
+  ]
+  );
+  alert("El numero de moles es : " + resultado);
+}
+
 function calcularGasesIdeales(){
+  $('#calcularPresionGasesIdeales').click(function(){
+    presionGasesIdeales();
+  });
+
+  $('#calcularVolumenGasesIdeales').click(function(){
+    volumenGasesIdeales();
+  });
+
+  $('#calcularTemperaturaGasesIdeales').click(function(){
+    temperaturaGasesIdeales();
+  });
+
+  $('#calcularNumeroMolesGasesIdeales').click(function(){
+    numeroMolesGasesIdeales();
+  });
+}
+
+/*function calcularGasesIdeales(){
 
   const R = 0.08205;
 
@@ -60,4 +136,4 @@ function calcularGasesIdeales(){
     }
   });
 
-}
+}*/
